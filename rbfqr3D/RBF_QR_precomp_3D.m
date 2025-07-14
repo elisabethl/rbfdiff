ee2a4c36a500eebd5bfcce0706d364e00589d866
Elisabeth Larsson <elisabeth.larsson@it.uu.se>
2025-07-14 20:04:54 +0200

    Fixed bug in ClenshawCheb and T_0

--- rbfqr3D/RBF_QR_precomp_3D.m
+++ rbfqr3D/RBF_QR_precomp_3D.m
@@ -7,7 +7,10 @@ n = size(xe,1);
 if nargin<7 || isempty(T)
     % Always compute degree zero variables, unless already present.
     T.xe = xe;
-    T.Tk = cos(acos(xe(:,1))*jvec);
+    
+    %T.Tk = cos(acos(xe(:,1))*jvec);
+    [T.Tk,T.Tk_p,T.Tk_b] = ClenshawCheb(xe(:,1),jmax);
+    T.R0 = Rjm0(T.Tk,T.Tk_p,xe(:,1));
     
     [T.Yk,T.Yth,T.Yfi_over_s,T.Yfi,T.Ythth,T.Ythfi,T.Yfifi] =  ...
         Y(jmax,xe(:,2),xe(:,3));
@@ -23,13 +26,13 @@ if T.deg<deg
     % Currently only degree zero or degree > 0
     rn1 = find(xe(:,1)+eps<1);
     T.deg = deg;
-    T.Tk_p = ones(n,1)*jvec.^2;
-    T.Tk_p(rn1,:) = repmat(jvec,length(rn1),1).*sin(acos(xe(rn1,1))*jvec)...
-        ./repmat(sin(acos(xe(rn1,1))),1,jmax+1);
-    T.Tk_b = ones(n,1)*(jvec.^2.*(jvec.^2-1)./3);
-    T.Tk_b(rn1,:) = (repmat(jvec.^2,length(rn1),1).*T.Tk(rn1,:)-...
-        repmat(xe(rn1,1),1,jmax+1).*T.Tk_p(rn1,:))./...
-        repmat(xe(rn1,1).^2-1,1,jmax+1);
+    %T.Tk_p = ones(n,1)*jvec.^2;
+    %T.Tk_p(rn1,:) = repmat(jvec,length(rn1),1).*sin(acos(xe(rn1,1))*jvec)...
+    %    ./repmat(sin(acos(xe(rn1,1))),1,jmax+1);
+    %T.Tk_b = ones(n,1)*(jvec.^2.*(jvec.^2-1)./3);
+    %T.Tk_b(rn1,:) = (repmat(jvec.^2,length(rn1),1).*T.Tk(rn1,:)-...
+    %    repmat(xe(rn1,1),1,jmax+1).*T.Tk_p(rn1,:))./...
+    %    repmat(xe(rn1,1).^2-1,1,jmax+1);
     
     M = length(m);
     T.f_over_r = zeros(n,M); T.f_over_r2 = zeros(n,M); T.f_prime = zeros(n,M);
@@ -61,8 +64,13 @@ if T.deg<deg
             else
                 T.f_over_r(:,k) = (-1)^((jk-1)/2)*jk*ones(n,1);
                 T.f_over_r(rn0,k) = T.Tk(rn0,jk-2*mk+1)./xe(rn0,1);
-                T.f_prime_over_r_minus_f_over_r2(rn0,k) = T.Tk_p(rn0,jk-2*mk+1)./xe(rn0,1)-...
-                    (2*ep^2+1./T.Pk(rn0,3)).*T.Tk(rn0,jk-2*mk+1);
+                
+                %T.f_prime_over_r_minus_f_over_r2(rn0,k) = ...
+                %    T.Tk_p(rn0,jk-2*mk+1)./xe(rn0,1)-...
+                %    (2*ep^2+1./T.Pk(rn0,3)).*T.Tk(rn0,jk-2*mk+1);
+                T.f_prime_over_r_minus_f_over_r2(rn0,k) = ...
+                    -2*ep^2*T.Tk(rn0,jk-2*mk+1) - T.R0(rn0,jk-2*mk+1);
+                
                 T.f_bis(:,k) = (4*ep^4*T.Pk(:,3) - 2*ep^2).*T.Tk(:,jk-2*mk+1) - ...
                     4*ep^2*xe(:,1).*T.Tk_p(:,jk-2*mk+1) + T.Tk_b(:,jk-2*mk+1);
             end
