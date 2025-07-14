25bea08a2d5e69abe747825840a8837936dcd44d
Andreas Michael <andreas.michael@it.uu.se>
2025-07-14 16:04:28 +0200

    LS-RBFFD done

--- examples/LSRBFFD.m
+++ examples/LSRBFFD.m
@@ -2,52 +2,51 @@ close all
 clear all
 setPaths;
 %
-% A least squares, unfitted RBF-FD example for solving the Poisson equation in a dim 
+% An unfitted least squares RBF-FD example for solving the Poisson equation in a dim 
 % dimensional ball using the available RBF routines.
 %
 dim = 2;                        % dim = 1,2 or 3
 scaling = 1;                    % Include scaling of the LS problem
+mvCentres = 0;                  % Option to have a Y point on top of all X points inside
 q = 2;                          % Oversampling
 h = 0.1;                        % approximate fill distance
 ep = 1;                         % Not relevant for 'r3' basis
-phi = 'r3';
-pdeg = 2;
-n = 2*nchoosek(pdeg+dim,dim);   % stencil size
-extCoeff = 0.5;
+phi = 'r3';                     % Choice of basis
+pdeg = 2;                       % Polynomial extension
+n = 2*nchoosek(pdeg+dim,dim);   % Stencil size
+extCoeff = 0.25;                % Extension size (in % of stencil)
 %
-% We place centre points in a line/circle/sphere extended by half a stencil
+% Place N centre points on line/circle/sphere, start with boundary if using fitted method
 %
 C = zeros(1,dim);
 R = 1;
-dimRat = [1 1.2*4/pi 1.2*8/(4*pi/3)];
-dimACoeff = [1 pi (4/3)*pi];
-dimPCoeff = [1 2*pi 4*pi];
+dimRat = [1 1.2*4/pi 1.2*8/(4*pi/3)];   % Ratios between rectangle/circle, cube/sphere area and volume
+dimACoeff = [1 pi (4/3)*pi];            % constant for size of domain
+dimPCoeff = [1 2*pi 4*pi];              % constant for computing size of boundary
 N = floor((R+n^(1/dim)*h*extCoeff)/(0.5*h))^dim;
-M = N*q;
-Ncube = ceil(dimRat(dim)*N);
+Ncube = ceil(dimRat(dim)*(N));
 xc = 2*(R+n^(1/dim)*h*extCoeff)*(halton(Ncube,dim)-0.5);
 r2 = sqrt(sum(xc.^2,2));
-pos = find(r2<=(R+n*h*extCoeff));
+pos = find(r2<=(R+n^(1/dim)*h*extCoeff));
 pos = pos(1:N);
 xcBlock = xc;
 xc = xc(pos,:) + C; 
 %
-% Place evaluation points. Start with boundary points
+% Place M evaluation points. Start with boundary points
 %
+M = N*q;
+hy = ((dimACoeff(dim)*R^dim)/M).^(1/dim);
+Mb = ceil((dimPCoeff(dim)*R^(dim-1))/(dimACoeff(dim-1)*hy^(dim-1)));
 if dim == 1
     xeB = [-R, R]';
 elseif dim == 2
-    approx_hy = ((pi*R^2)/(N*q)).^(1/dim);
-    Nb = ceil(2*pi*R/approx_hy);
-    xeB = [R*cos(linspace(0,2*pi,Nb)'), R*sin(linspace(0,2*pi,Nb)')];
+    xeB = [R*cos(linspace(-pi,pi,Mb)'), R*sin(linspace(-pi,pi,Mb)')];
 elseif dim == 3
     % Fibonacci points on sphere
-    approx_hy = ((pi*R^2)/(N*q)).^(1/dim);
-    Nb = ceil(4*pi*R/approx_hy);
     ratio = 1+sqrt(5);
-    ind = [0:Nb-1]' + 0.5;
+    ind = [0:Mb-1]' + 0.5;
     theta = pi*ratio*ind;
-    fi = acos(1-(2*ind)/(Nb));
+    fi = acos(1-(2*ind)/(Mb));
     xeB = [R.*sin(fi).*cos(theta), R.*sin(fi).*sin(theta), R.*cos(fi)];
 end
 Mb = length(xeB);
@@ -61,22 +60,24 @@ Min = length(xe);
 %
 % Move evaluation points on the inside to the closest centre point
 %
-xcIn = xc(sqrt(sum(xc.^2,2))<=R,:);
-xeTemp = xe;
-for i = 1:length(xcIn)
-    idMove = knnsearch(xeTemp,xcIn(i,:),'K',1);
-    xe(idMove,:) = xcIn(i,:);
-    xeTemp(idMove,:) = R.*ones(1,dim);
+if mvCentres
+    xcIn = xc(sqrt(sum(xc.^2,2))<=R,:);
+    xeTemp = xe;
+    for i = 1:length(xcIn)
+        idMove = knnsearch(xeTemp,xcIn(i,:),'K',1);
+        xe(idMove,:) = xcIn(i,:);
+        xeTemp(idMove,:) = R.*ones(1,dim);
+    end
+    idMove = knnsearch(xe,xcIn,'K',1);
+    xe(idMove,:) = xcIn;
 end
-idMove = knnsearch(xe,xcIn,'K',1);
-xe(idMove,:) = xcIn;
 xeAll = [xe; xeB]; % Interior and boundary points (all centres for collocation)
 %
 % Ensure center points are not too far from boundary and make evaluation point - stencil list 
 %
 xc = xc(unique(knnsearch(xc,xeAll,'K',ceil(extCoeff*n))),:);
-ptStencilList = knnsearch(xc,xeAll,'K',1);
 N = length(xc);
+ptStencilList = knnsearch(xc,xeAll,'K',1);
 %
 % Constructing global LS-RBF-FD approximation to evaluation, Laplace and boundary operators Min x N
 %
@@ -90,14 +91,13 @@ for i = 1:N
     Psi = RBFInterpMat(phi,pdeg,ep,xcLoc,xcLoc(1,:),max(sqrt(sum((xcLoc-xcLoc(1,:)).^2,2))));
     idInt = find(ptStencilList(1:Min)==i);
     idBnd = find(ptStencilList(Min+1:M)==i);
+    % Compute evaluation, Laplace and boundary operators
     E = RBFDiffMat(0,Psi,xeAll([idInt; idBnd+Min],:));
-    Ltest = RBFDiffMat(1.5,Psi,xeAll([idInt; idBnd+Min],:));
     L = RBFDiffMat(1.5,Psi,xeAll(idInt,:));
     B = RBFDiffMat(0,Psi,xeAll(idBnd+Min,:));
     
     Eglobal([idInt; idBnd+Min],id) = E + Eglobal([idInt; idBnd+Min],id);
     Lglobal(idInt,id) = L + Lglobal(idInt,id);
-    LtestGlob([idInt; idBnd+Min],id) = Ltest + LtestGlob([idInt; idBnd+Min],id);
     Bglobal(idBnd,id) = B + Bglobal(idBnd,id);
 end
 %
@@ -110,6 +110,7 @@ if dim == 1
     uExact = fun(xeAll(:,1));
     ucExact = fun(xc(:,1));
     lapAnalytic = lapFun(xe(:,1));
+    bndAnalytic = [0; 0];
 elseif dim == 2
     fun = @(x,y) sin(2.*pi.*x.*y);
     lapFun = @(x,y) - 4.*(x.^2).*(pi.^2).*sin(2.*pi.*x.*y) - 4.*(y.^2).*(pi.^2).*sin(2.*pi.*x.*y);
@@ -117,6 +118,7 @@ elseif dim == 2
     uExact = fun(xeAll(:,1),xeAll(:,2));
     ucExact = fun(xc(:,1),xc(:,2));
     lapAnalytic = lapFun(xe(:,1),xe(:,2));
+    bndAnalytic = fun(xeB(:,1),xeB(:,2));
 elseif dim == 3
     fun = @(x,y,z) sin(2.*pi.*x.*y.*z);
     lapFun = @(x,y,z) - 4.*(x.^2).*(y.^2).*(pi.^2).*sin(2.*pi.*x.*y.*z) - 4.*(y.^2).*(z.^2).*(pi.^2).*sin(2.*pi.*x.*y.*z) - 4.*(x.^2).*(z.^2).*(pi.^2).*sin(2.*pi.*x.*y.*z);
@@ -124,31 +126,29 @@ elseif dim == 3
     uExact = fun(xeAll(:,1),xeAll(:,2),xeAll(:,3));
     ucExact = fun(xc(:,1),xc(:,2),xc(:,3));
     lapAnalytic = lapFun(xe(:,1),xe(:,2),xe(:,3));
+    bndAnalytic = fun(xeB(:,1),xeB(:,2),xeB(:,3));
 end 
-
-if scaling == 1
+%
+% LS problem scaling
+%
+if scaling
     Lscale = sqrt((dimACoeff(dim)*R^dim)/Min);
     Bscale =sqrt((dimPCoeff(dim)*R^(dim-1))/Mb)*(h.^-1.5);
-    % Lscale = sqrt(1/Min);
-    % Bscale = sqrt(1/Mb)*(h.^-1);
-    A = [Lscale.*Lglobal; Bscale.*Bglobal];
-    F(1:Min) = Lscale.*F(1:Min);
-    F(Min+1:M) = Bscale.*F(Min+1:M);
-elseif scaling == 2
-    Lscale = sqrt(sum(Lglobal.^2,2));
-    Bscale = sqrt(sum(Bglobal.^2,2));
     A = [Lscale.*Lglobal; Bscale.*Bglobal];
     F(1:Min) = Lscale.*F(1:Min);
     F(Min+1:M) = Bscale.*F(Min+1:M);
 else
     A = [Lglobal; Bglobal];
 end
-
+%
+% Solution on centre points, evaluated on Y set
+%
 u = A\F;
 ue = Eglobal*u;
-
 error = abs(ue-uExact);
-
+%
+% Plotting
+%
 if dim == 1
     figure()
     plot(xeAll,uExact,'.'); 
@@ -194,12 +194,14 @@ elseif dim == 3
     colorbar
     title("Error")
 end
-
+%
+% Operator and solution l2 errors
+%
 lapNumeric = Lglobal*ucExact;
 evalNumeric = Bglobal*ucExact;
 l2Error = norm(error,2)/norm(uExact,2);
 laplaceError = norm(lapAnalytic-lapNumeric,2)/norm(lapAnalytic,2);
-bndError = norm(evalNumeric-fun(xeB(:,1),xeB(:,2)),2)/norm(fun(xeB(:,1),xeB(:,2)),2);
+bndError = norm(evalNumeric-bndAnalytic,2)/(norm(bndAnalytic,2) + double(max(abs(bndAnalytic))==0));
 disp(['PDE error = ', num2str(l2Error)]);
-disp(['Boundary error = ', num2str(bndError)]);
-disp(['Laplace error = ', num2str(laplaceError)]);
+disp(['Boundary Op error = ', num2str(bndError)]);
+disp(['Laplace Op error = ', num2str(laplaceError)]);
