29a6fccc2b16f15ad30fb27be56e154d094ee6b2
Andreas Michael <andreas.michael@it.uu.se>
2025-07-16 19:59:14 +0200

    fitted LSRBFFD

--- examples/LSRBFFD.m
+++ examples/LSRBFFD.m
@@ -5,110 +5,85 @@ setPaths;
 % An unfitted least squares RBF-FD example for solving the Poisson equation in a dim 
 % dimensional ball using the available RBF routines.
 %
-dim = 1;                            % dim = 1,2 or 3
+dim = 2;                            % dim = 1,2 or 3
 display = 1;                        % Plot solution
-domain = 'ball';                    % ball or cube
+geom = 'ball';                      % ball or cube
+mode = 'unfitted';                    % fitted, unfitted or collocation
 scaling = 1;                        % Include scaling of the LS problem
-mvCentres = 1;                      % Option to have a Y point on top of all X points inside
+mvCentres = 0;                      % Option to have a Y point on top of all X points inside the domain
 q = 2;                              % Oversampling
-h = 0.01;                            % approximate fill distance
+N = 200;                            % Number of center points
 ep = 1;                             % Not relevant for 'r3' basis
 phi = 'r3';                         % Choice of basis 
-pdeg = 2;                           % Polynomial extension
+pdeg = 2;                           % Polynomial extension, not relevant for 'rbfqr'
+rbfDeg = 4;                         % Relevant when there is no polynomial extension
+extCoeff = 0.5;                     % Extension size (in % of stencil), relevant for unfitted method
 if pdeg == -1
-    n = 2*nchoosek(5+dim,dim);
+    n = nchoosek(rbfDeg+dim-1,dim); % Order in smooth RBF case
 else
     n = 2*nchoosek(pdeg+dim,dim);   % Stencil size
 end
-extCoeff = 0;                     % Extension size (in % of stencil)
+extCoeff = extCoeff*(strcmp(mode,"unfitted"));
 %
-% Place N centre points on line/circle/sphere, start with boundary if using fitted method
+% Place N centre points and M evaluation points in geom with centre C and
+% radius R
 %
 C = zeros(1,dim);
 R = 1;
-dimRat = [1 1.2*4/pi 1.2*8/(4*pi/3)];   % Ratios between rectangle/circle, cube/sphere area and volume
-dimACoeff = [1 pi (4/3)*pi];            % constant for size of domain
-dimPCoeff = [1 2*pi 4*pi];              % constant for computing size of boundary
-N = floor((R+n^(1/dim)*h*extCoeff)/(0.5*h))^dim;
-Ncube = ceil(dimRat(dim)*(N));
-xc = 2*(R+n^(1/dim)*h*extCoeff)*(halton(Ncube,dim)-0.5);
-r2 = sqrt(sum(xc.^2,2));
-pos = find(r2<=(R+n^(1/dim)*h*extCoeff));
-pos = pos(1:N);
-xcBlock = xc;
-xc = xc(pos,:) + C; 
+[xc,xcB,V,Area] = getPts(geom,N,n,C,R,mode,extCoeff);
+xcAll = [xc; xcB];                              % Interior and boundary points 
+idXin = 1:size(xc,1);                           % Interior pts index
+idXbnd = size(xc,1)+1:size(xcAll,1);              % Boundary pts index
+% 
+% For collocation, evaluation and centre points match
 %
-% Place M evaluation points. Start with boundary points
-%
-M = ceil(N*q);
-hy = ((dimACoeff(dim)*R^dim)/M).^(1/dim);
-if dim == 1
-    xeB = [-R, R]';
-elseif dim == 2
-    Mb = ceil((dimPCoeff(dim)*R^(dim-1))/(dimACoeff(dim-1)*hy^(dim-1)));
-    xeB = [R*cos(linspace(-pi,pi,Mb)'), R*sin(linspace(-pi,pi,Mb)')];
-elseif dim == 3
-    Mb = ceil((dimPCoeff(dim)*R^(dim-1))/(dimACoeff(dim-1)*hy^(dim-1)));
-    % Fibonacci points on sphere
-    ratio = 1+sqrt(5);
-    ind = [0:Mb-1]' + 0.5;
-    theta = pi*ratio*ind;
-    fi = acos(1-(2*ind)/(Mb));
-    xeB = [R.*sin(fi).*cos(theta), R.*sin(fi).*sin(theta), R.*cos(fi)];
-end
-Mb = length(xeB);
-Ncube = ceil(dimRat(dim)*(M-Mb));
-xe = 2*R*(halton(Ncube,dim)-0.5);
-r2 = sqrt(sum(xe.^2,2));
-pos = find(r2<=R);
-pos = pos(1:(M-Mb));
-xe = xe(pos,:) + C; 
-Min = length(xe);
-%
-% Move evaluation points on the inside to the closest centre point
-%
-if mvCentres
-    xcIn = xc(sqrt(sum(xc.^2,2))<=R,:);
-    xeTemp = xe;
-    for i = 1:length(xcIn)
-        idMove = knnsearch(xeTemp,xcIn(i,:),'K',1);
-        xe(idMove,:) = xcIn(i,:);
-        xeTemp(idMove,:) = R.*ones(1,dim);
+if strcmp(mode,"collocation")
+    xe = xc; xeB = xcB;
+else
+    M = q*N;
+    [xe,xeB] = getPts(geom,M,n,C,R,"fitted",0);
+    if mvCentres
+        xcIn = xc(sqrt(sum(xc.^2,2))<=R,:);
+        xeTemp = xe;
+        for i = 1:length(xcIn)
+            idMove = knnsearch(xeTemp,xcIn(i,:),'K',1);
+            xe(idMove,:) = xcIn(i,:);
+            xeTemp(idMove,:) = 2*R.*ones(1,dim);
+        end
+        idMove = knnsearch(xe,xcIn,'K',1);
+        xe(idMove,:) = xcIn;
     end
-    idMove = knnsearch(xe,xcIn,'K',1);
-    xe(idMove,:) = xcIn;
 end
-xeAll = [xe; xeB]; % Interior and boundary points (all centres for collocation)
+xeAll = [xe; xeB];                  % Interior and boundary points 
+idYin = 1:size(xe,1);               % Interior pts index
+idYbnd = size(xe,1)+1:size(xeAll,1);  % Boundary pts index
 %
 % Ensure center points are not too far from boundary and make evaluation point - stencil list 
 %
-if extCoeff ~= 0
-    xc = xc(unique(knnsearch(xc,xeAll,'K',ceil(extCoeff*n))),:);
+if strcmp(mode,"unfitted")
+    xcAll = xcAll(unique(knnsearch(xcAll,xeAll,'K',ceil(extCoeff*n))),:);
 end
-N = length(xc);
-ptStencilList = knnsearch(xc,xeAll,'K',1);
+N = length(xcAll);
+ptStencilList = knnsearch(xcAll,xeAll,'K',1);
+relXc = unique(ptStencilList);
 %
-% Constructing global LS-RBF-FD approximation to evaluation, Laplace and boundary operators Min x N
+% Constructing global LS-RBF-FD approximation to evaluation and Laplace operators M x N
 %
 Eglobal = spalloc(M,N,M*n);
-Lglobal = spalloc(Min,N,Min*n);
-LtestGlob = spalloc(M,N,M*n);
-Bglobal = spalloc(Mb,N,Mb*n);
-for i = 1:N
-    [id,~] = knnsearch(xc,xc(i,:),'K',n);
-    xcLoc = xc(id,:); % stencil
+Lglobal = spalloc(M,N,M*n);
+for i = 1:length(relXc)
+    [idX,~] = knnsearch(xcAll,xcAll(relXc(i),:),'K',n);
+    xcLoc = xcAll(idX,:); % stencil
     Psi = RBFInterpMat(phi,pdeg,ep,xcLoc,xcLoc(1,:),max(sqrt(sum((xcLoc-xcLoc(1,:)).^2,2))));
-    idInt = find(ptStencilList(1:Min)==i);
-    idBnd = find(ptStencilList(Min+1:M)==i);
-    % Compute evaluation, Laplace and boundary operators
-    E = RBFDiffMat(0,Psi,xeAll([idInt; idBnd+Min],:));
-    L = RBFDiffMat(1.5,Psi,xeAll(idInt,:));
-    B = RBFDiffMat(0,Psi,xeAll(idBnd+Min,:));
+    idY = find(ptStencilList==relXc(i));
+    E = RBFDiffMat(0,Psi,xeAll(idY,:));
+    L = RBFDiffMat(1.5,Psi,xeAll(idY,:));
     
-    Eglobal([idInt; idBnd+Min],id) = E + Eglobal([idInt; idBnd+Min],id);
-    Lglobal(idInt,id) = L + Lglobal(idInt,id);
-    Bglobal(idBnd,id) = B + Bglobal(idBnd,id);
+    Eglobal(idY,idX) = E + Eglobal(idY,idX);
+    Lglobal(idY,idX) = L + Lglobal(idY,idX);
 end
+L = Lglobal(idYin,:);
+B = Eglobal(idYbnd,:);
 %
 % Manufactured solution to construct forcing and BC
 %
@@ -117,7 +92,7 @@ if dim == 1
     lapFun = @(x) -4.*(pi.^2).*sin(2.*pi.*x);
     F = [lapFun(xe(:,1)); fun(xeB(:,1))];
     uExact = fun(xeAll(:,1));
-    ucExact = fun(xc(:,1));
+    ucExact = fun(xcAll(:,1));
     lapAnalytic = lapFun(xe(:,1));
     bndAnalytic = [0; 0];
 elseif dim == 2
@@ -125,7 +100,7 @@ elseif dim == 2
     lapFun = @(x,y) - 4.*(x.^2).*(pi.^2).*sin(2.*pi.*x.*y) - 4.*(y.^2).*(pi.^2).*sin(2.*pi.*x.*y);
     F = [lapFun(xe(:,1),xe(:,2)); fun(xeB(:,1),xeB(:,2))];
     uExact = fun(xeAll(:,1),xeAll(:,2));
-    ucExact = fun(xc(:,1),xc(:,2));
+    ucExact = fun(xcAll(:,1),xcAll(:,2));
     lapAnalytic = lapFun(xe(:,1),xe(:,2));
     bndAnalytic = fun(xeB(:,1),xeB(:,2));
 elseif dim == 3
@@ -133,43 +108,63 @@ elseif dim == 3
     lapFun = @(x,y,z) - 4.*(x.^2).*(y.^2).*(pi.^2).*sin(2.*pi.*x.*y.*z) - 4.*(y.^2).*(z.^2).*(pi.^2).*sin(2.*pi.*x.*y.*z) - 4.*(x.^2).*(z.^2).*(pi.^2).*sin(2.*pi.*x.*y.*z);
     F = [lapFun(xe(:,1),xe(:,2),xe(:,3)); fun(xeB(:,1),xeB(:,2),xeB(:,3))];
     uExact = fun(xeAll(:,1),xeAll(:,2),xeAll(:,3));
-    ucExact = fun(xc(:,1),xc(:,2),xc(:,3));
+    ucExact = fun(xcAll(:,1),xcAll(:,2),xcAll(:,3));
     lapAnalytic = lapFun(xe(:,1),xe(:,2),xe(:,3));
     bndAnalytic = fun(xeB(:,1),xeB(:,2),xeB(:,3));
 end 
+
 %
 % LS problem scaling
 %
 if scaling
-    Lscale = sqrt((dimACoeff(dim)*R^dim)/Min);
-    Bscale =sqrt((dimPCoeff(dim)*R^(dim-1))/Mb)*(h.^-1.5);
-    A = [Lscale.*Lglobal; Bscale.*Bglobal];
-    F(1:Min) = Lscale.*F(1:Min);
-    F(Min+1:M) = Bscale.*F(Min+1:M);
-else
-    A = [Lglobal; Bglobal];
+    [~,dist] = knnsearch(xcAll,xcAll,'K',2);
+    h = max(dist(:,2));
+    Lscale = sqrt(V/length(idYin));
+    Bscale = sqrt(Area/length(idYbnd))*(h.^-1.5);
+    F(idYin) = Lscale.*F(idYin);
+    F(idYbnd) = Bscale.*F(idYbnd);
 end
+
+if strcmp(mode,"fitted")
+    Fmod = Lglobal(:,idXbnd)*ucExact(idXbnd);
+    F = F-Fmod;
+    L = Lglobal(:,idXin);
+    B = zeros(0,N);
+end
+
 %
 % Solution on centre points, evaluated on Y set
 %
+A = [L; B];
 u = A\F;
-ue = Eglobal*u;
 
-if display
-    plotSolution(ue,uExact,xeAll,xeB,dim);
+%
+% Fix operators to compute error measures
+%
+if strcmp(mode,"fitted")
+    u = [u; ucExact(idXbnd)];
+    L = Lglobal(idYin,:);
+    B = Eglobal(idYbnd,:);
 end
-
+ue = Eglobal*u;
 %
-% Operator and solution l2 errors
+% Displaying output 
 %
-lapNumeric = Lglobal*ucExact;
-evalNumeric = Bglobal*ucExact;
-l2Error = norm(abs(ue-uExact),2)/norm(uExact,2);
-laplaceError = norm(lapAnalytic-lapNumeric,2)/norm(lapAnalytic,2);
-bndError = norm(evalNumeric-bndAnalytic,2)/(norm(bndAnalytic,2) + double(max(abs(bndAnalytic))==0));
-disp(['PDE error = ', num2str(l2Error)]);
-disp(['Boundary Op error = ', num2str(bndError)]);
-disp(['Laplace Op error = ', num2str(laplaceError)]);
+if display
+    % Plotting 
+    plotSolution(ue,uExact,xeAll,xeB,dim);
+    %
+    % Operator and solution l2 errors
+    %
+    lapNumeric = L*ucExact;
+    evalNumeric = B*ucExact;
+    l2Error = norm(abs(ue-uExact),2)/norm(uExact,2);
+    laplaceError = norm(lapAnalytic-lapNumeric,2)/norm(lapAnalytic,2);
+    bndError = norm(evalNumeric-bndAnalytic,2)/(norm(bndAnalytic,2) + double(max(abs(bndAnalytic))==0));
+    disp(['PDE error = ', num2str(l2Error)]);
+    disp(['Boundary Op error = ', num2str(bndError)]);
+    disp(['Laplace Op error = ', num2str(laplaceError)]);
+end
 
 %
 % Plotting routines
@@ -177,20 +172,25 @@ disp(['Laplace Op error = ', num2str(laplaceError)]);
 function [] = plotSolution(uNumeric,uAnalytic,x,xB,dim)
     error = uNumeric-uAnalytic;
     if dim == 1
+        [x,id] = sort(x);
         figure()
-        plot(x,uAnalytic,'b-'); 
+        plot(x,uAnalytic(id),'r-'); 
+        hold on;
+        plot(x,uNumeric(id),'bo');
         ax = gca;
         ax.FontSize = 18;
         xlabel("x","Interpreter","latex","FontSize",24)
-        ylabel("$$u_E$$","Interpreter","latex","FontSize",24,'Rotation',0)
+        ylabel("$$u$$","Interpreter","latex","FontSize",24,'Rotation',0)
+        legend('$u_E$','$u_N$','FontSize',18,'Interpreter','latex');
+        grid on
     
         figure()
-        plot(x,uNumeric,'.');
-        title("Numerical PDE solution")
-    
-        figure()
-        plot(x,error,'.');
-        title("Error")
+        plot(x,error(id),'b-.');
+        ax = gca;
+        ax.FontSize = 18;
+        xlabel("x","Interpreter","latex","FontSize",24)
+        ylabel("$e_{rel}$","Interpreter","latex","FontSize",24,'Rotation',0)
+        grid on
     elseif dim == 2
         T = delaunay(x(:,1),x(:,2));
 
@@ -229,22 +229,90 @@ function [] = plotSolution(uNumeric,uAnalytic,x,xB,dim)
         zlabel("$$u-u_E$$","Interpreter","latex","FontSize",24)
         set(G,'EdgeColor','none')
         shading interp
-    
     elseif dim == 3
+
+        bndPtCloud = pointCloud(xB);
+        surfMesh = pc2surfacemesh(bndPtCloud,"ball-pivot");
+        x = surfMesh.Vertices;
+        T = surfMesh.Faces;
+        
         figure()
-        scatter3(x(:,1),x(:,2),x(:,3),[],uAnalytic,'filled'); axis equal
-        colorbar
-        title("Exact solution")
+        G=trisurf(T,x(:,1),x(:,2),x(:,3),uAnalytic(end-length(xB)+1:end));
+        axis equal
+        ax = gca;
+        ax.FontSize = 18;
+        xlabel("x","Interpreter","latex","FontSize",24)
+        ylabel("y","Interpreter","latex","FontSize",24)
+        zlabel("z","Interpreter","latex","FontSize",24,"Rotation",0)
+        set(G,'EdgeColor','none')
+        cb = colorbar;
+        ylabel(cb,"$$u_E$$","Interpreter","latex","FontSize",24,'Rotation',0)
+        shading interp
+        
+        figure()
+        G=trisurf(T,x(:,1),x(:,2),x(:,3),uNumeric(end-length(xB)+1:end));
+        axis equal
+        ax = gca;
+        ax.FontSize = 18;
+        xlabel("x","Interpreter","latex","FontSize",24)
+        ylabel("y","Interpreter","latex","FontSize",24)
+        zlabel("z","Interpreter","latex","FontSize",24,"Rotation",0)
+        set(G,'EdgeColor','none')
+        cb = colorbar;
+        ylabel(cb,"$$u$$","Interpreter","latex","FontSize",24,'Rotation',0)
+        shading interp
     
         figure()
-        scatter3(x(:,1),x(:,2),x(:,3),[],uNumeric,'filled'); axis equal
-        colorbar
-        title("Numerical solution")
-    
-        figure()
-        scatter3(x(:,1),x(:,2),x(:,3),[],error,'filled'); axis equal
-        colorbar
-        title("Error")
+        G=trisurf(T,x(:,1),x(:,2),x(:,3),error(end-length(xB)+1:end));
+        axis equal
+        ax = gca;
+        ax.FontSize = 18;
+        xlabel("x","Interpreter","latex","FontSize",24)
+        ylabel("y","Interpreter","latex","FontSize",24)
+        zlabel("z","Interpreter","latex","FontSize",24,"Rotation",0)
+        set(G,'EdgeColor','none')
+        cb = colorbar;
+        ylabel(cb,"$$u-u_E$$","Interpreter","latex","FontSize",24,'Rotation',90)
+        shading interp
+    end
+end
+function [x,xB,Vol,Area] = getPts(geom,N,n,C,R,mode,extCoeff)
+    dim = size(C,2);
+    if strcmp(geom,'ball')
+        dimRat = [1 1.2*4/pi 1.2*8/(4*pi/3)];       % Ratios between rectangle/circle, cube/sphere area and volume
+        dimACoeff = [1 pi (4/3)*pi];                % constant for size of domain
+        dimPCoeff = [1 2*pi 4*pi];                  % constant for computing size of boundary
+        h = R/(0.5*N^(1/dim) - extCoeff*n^(1/dim)); % approximate fill distance
+        xB = zeros(0,dim);
+        %
+        % If using theunfitted method, no points on the boundary
+        %
+        if ~strcmp(mode,'unfitted')
+            if dim == 1
+                xB = [-R, R]';
+            elseif dim == 2
+                Nb = ceil((dimPCoeff(dim)*R^(dim-1))/(dimACoeff(dim-1)*h^(dim-1)));
+                xB = [R*cos(linspace(-pi,pi-(2*pi)/(Nb+1),Nb)'), R*sin(linspace(-pi,pi-(2*pi)/(Nb+1),Nb)')];
+            elseif dim == 3
+                Nb = ceil((dimPCoeff(dim)*R^(dim-1))/(dimACoeff(dim-1)*(0.5*h)^(dim-1)));
+                % Fibonacci points on sphere
+                ratio = 1+sqrt(5);
+                ind = [0:Nb-1]' + 0.5;
+                theta = pi*ratio*ind;
+                fi = acos(1-(2*ind)/(Nb));
+                xB = [R.*sin(fi).*cos(theta), R.*sin(fi).*sin(theta), R.*cos(fi)];
+            end
+        end
+        Nb = length(xB);
+        Ncube = ceil(dimRat(dim)*(N-Nb));
+        x = 2*(R+n^(1/dim)*h*extCoeff)*(halton(Ncube,dim)-0.5);
+        r2 = sqrt(sum(x.^2,2));
+        pos = find(r2<=(R+n^(1/dim)*h*extCoeff));
+        pos = pos(1:N-Nb);
+        x = x(pos,:) + C;
+
+        Vol = (dimACoeff(dim)*R^dim);
+        Area = (dimPCoeff(dim)*R^(dim-1));
     end
 end
 
