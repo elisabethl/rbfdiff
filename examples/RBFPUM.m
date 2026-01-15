fbbcb95d4ccdb1c93db2810a1b09713964c33e79
Andreas Michael <andreas.michael@it.uu.se>
2026-01-15 15:02:48 +0100

    Fixing patch generation for circle and sphere

--- examples/RBFPUM.m
+++ examples/RBFPUM.m
@@ -5,16 +5,16 @@ setPaths;
 % An RBF-FD example for solving the Poisson equation. Collocation and LS
 % unfitted or fitted methods.
 %
-dim = 3;                            % dim = 1,2 or 3
+dim = 2;                            % dim = 1,2 or 3
 display = 1;                        % Plot solution
-geom = 'cube';                      % ball or cube
+geom = 'ball';                      % ball or cube
 mode = 'unfitted';                    % fitted, unfitted or collocation
 bcMode = 'weak';                    % strong or weak imposition of boundary conditions (only relevant for fitted)
 scaling = 1;                        % Include scaling of the unfitted LS problem
 mvCentres = 1;                      % Option to have a Y point on top of all X points inside the domain
 q = 2;                              % Oversampling
-N = 56;                             % Number of center points (X) in each patch
-P = 27;                             % Number of patches
+N = 20;                             % Number of center points (X) in each patch
+P = 45;                             % Number of patches
 
 ep = 0.1;                           % Not relevant for 'r3' basis
 phi = 'rbfqr';                      % Choice of basis 'r3', 'mq', 'gs', 'iq', 'rbfqr'
@@ -568,7 +568,36 @@ function ptch = getPtch(geom,P,C,R,del)
     end
     ptch.R = ptch.R*ones(size(ptch.C,1),1);
     if strcmp(geom,"ball")
-        idBall = find(sum((ptch.C - C).^2,2)<=(R+(0.5-del).*ptch.R).^2);
+        tol = 1e-6;
+        % Get closest patch neighbours based on distance
+        [idNei,distNei] = rangesearch(ptch.C,ptch.C,(2*ptch.R(1)-ptch.R(1)*del) + ptch.R(1)*tol);
+        % Find points of intersection between patches
+        edgePts = [];
+        for i = 1:size(idNei,1)
+            neiTemp = idNei{i};
+            distTemp = distNei{i};
+            for j = 1:length(neiTemp)-1
+                t = (ptch.C(neiTemp(1),:) + ptch.C(neiTemp(j+1),:))/2;
+                t2 = (-ptch.C(neiTemp(1),:) + ptch.C(neiTemp(j+1),:))/norm((-ptch.C(neiTemp(1),:) + ptch.C(neiTemp(j+1),:)),2);
+                t2 = [t2(2), -t2(1)];
+                if distTemp(j+1) < (2*ptch.R(1) - del*ptch.R(1)) - tol*ptch.R(1)
+                    edgePts = [edgePts; ptch.R(1)*sin(acos((sqrt(2)/4)*(2-del)))*(t2) + t];
+                    edgePts = [edgePts; -ptch.R(1)*sin(acos((sqrt(2)/4)*(2-del)))*(t2) + t];
+                else
+                    edgePts = [edgePts; ptch.R(1)*sin(acos(1-del/2))*(t2) + t];
+                    edgePts = [edgePts; -ptch.R(1)*sin(acos(1-del/2))*(t2) + t];
+                end
+            end
+        end
+        edgePts= uniquetol(edgePts,tol,'ByRows',true);
+        % Make sure that points of intersection that are close to or inside
+        % the domain are fully covered. This guarantees no part of the
+        % boundary is left uncovered.
+        idEdgeIn = find(sum((edgePts - C).^2,2)<=(R+del.*ptch.R(1)).^2);
+        for i = 1:length(edgePts)
+            ptPtchList(i,:) = sqrt(sum((edgePts(i,:) - ptch.C).^2,2)) < ptch.R - ptch.R.*tol;
+        end
+        idBall = find(sum(ptPtchList(idEdgeIn,:),1));
         ptch.C = ptch.C(idBall,:);
         ptch.R = ptch.R(idBall,:);
     end
